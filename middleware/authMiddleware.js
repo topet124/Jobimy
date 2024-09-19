0ab5e52764f589357151646548a10c8f05197b62
Tope  Sule <57067649+topet124@users.noreply.github.com>
2024-09-19 09:13:04 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,27 +3,22 @@ import {
   UnauthorizedError,
   BadRequestError,
 } from "../errors/customErrors.js";
-import { verifyJWT } from "../utils/tokenUtils.js";
+// import { verifyJWT } from "../utils/tokenUtils.js"; 
 
 export const authenticateUser = (req, res, next) => {
-  const { token } = req.cookies;
-  if (!token)
-    throw new UnauthenticatedError("authentication invalid token based");
+  // Bypass token verification
+  req.user = { 
+    userId: "mockUserId", // Assign a mock user ID
+    role: "user", // Assign a mock role, adjust as needed
+    testUser: true // You can set this to false or true
+  };
 
-  try {
-    const { userId, role } = verifyJWT(token);
-    const testUser = userId === "66e64906f265b9a66020daad";
-    req.user = { userId, role, testUser };
-
-    next();
-  } catch (error) {
-    throw new UnauthenticatedError("authentication invalid user");
-  }
+  next();
 };
 
 export const authorizePermissions = (...roles) => {
   return (req, res, next) => {
-    if (!roles.includes(req.user.roles)) {
+    if (!roles.includes(req.user.role)) {
       throw new UnauthorizedError("Unauthorized to access this route");
     }
     next();
