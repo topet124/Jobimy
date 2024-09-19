49747de7debffe6118c4b429ad1a42918d6cc45c
Tope  Sule <57067649+topet124@users.noreply.github.com>
2024-09-19 09:31:57 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-import User from '../models/User.js';
+import User from '../models/UserModel.js';
 export const getCurrentUser = async (req, res, next) => {
   try {
     const user = await User.findById(req.user.userId);
